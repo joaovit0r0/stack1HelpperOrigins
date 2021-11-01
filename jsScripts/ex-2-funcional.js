af5968fdc7c191d2886bd7d9b48b55ae9f2a5702
joaovit0r0 <joao.teixeira2@estudante.ufla.br>
2021-11-01 13:02:20 -0300

    adiciona ponto e virgula e separando imperativo e funcional

--- jsScripts/ex-2-funcional.js
+++ jsScripts/ex-2-funcional.js
@@ -10,6 +10,7 @@ var lista = [
     { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
     { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
+// verifica se o id passado é válido
 var verificaIndice = function (identificador) {
     if ((lista.findIndex(function (objeto) { return objeto.id === identificador; })) != -1) {
         return true;
@@ -18,9 +19,6 @@ var verificaIndice = function (identificador) {
         return false;
     }
 };
-// Teste aqui QA
-// console.log("Crie uma função que retorne a bio do id passado \n")
-// Funcional
 var pegaBio = function (identificador) {
     if (verificaIndice(identificador)) {
         var objeto = lista.find(function (objeto) { return objeto.id === identificador; });
@@ -30,27 +28,7 @@ var pegaBio = function (identificador) {
         return "Id inválido!";
     }
 };
-// Teste aqui QA
-// console.log("Funcional: ",pegaBio(1), "\n")
-// Imperativo
-var pegaBioImperativo = function (identificador) {
-    if (verificaIndice(identificador)) {
-        for (var i = 0; i < lista.length; i++) {
-            if (lista[i].id === identificador) {
-                return lista[i].bio;
-            }
-        }
-    }
-    else {
-        return "Id inválido!";
-    }
-};
-// Teste aqui QA
-// console.log("Imperativo: ", pegaBioImperativo(1))
-// console.log("---------------------\n")
-// Teste aqui QA
-// console.log("Crie uma função que retorne o name do id passado\n")
-// Funcional
+console.log(pegaBio(1));
 var pegaNome = function (identificador) {
     if (verificaIndice(identificador)) {
         var objeto = lista.find(function (objeto) { return objeto.id === identificador; });
@@ -60,27 +38,7 @@ var pegaNome = function (identificador) {
         return "Id inválido!";
     }
 };
-// Teste aqui QA
-// console.log("Funcional: ",pegaNome(1), "\n")
-// Imperativo
-var pegaNomeImperativo = function (identificador) {
-    if (verificaIndice(identificador)) {
-        for (var i = 0; i < lista.length; i++) {
-            if (lista[i].id === identificador) {
-                return lista[i].name;
-            }
-        }
-    }
-    else {
-        return "Id inválido!";
-    }
-};
-// Teste aqui QA
-// console.log("Imperativo: ", pegaNomeImperativo(1))
-// console.log("---------------------\n")
-// Teste aqui QA
-console.log("Crie uma função que apague um item da lista a partir de um id passado");
-// Funcional
+console.log(pegaNome(1));
 var deletaItem = function (identificador) {
     // indice necessário, por isso a não utilização da verificaInidice
     var indice = lista.findIndex(function (objeto) { return objeto.id === identificador; });
@@ -91,28 +49,8 @@ var deletaItem = function (identificador) {
         console.log("Id inválido!");
     }
 };
-// Teste aqui QA
-// deletaItem(1)
-// console.log("Funcional:\n", lista)
-// Imperativo
-var deletaItemImperativo = function (identificador) {
-    if (verificaIndice(identificador)) {
-        for (var i = 0; i < lista.length; i++) {
-            if (identificador === lista[i].id) {
-                lista.splice(i, 1);
-            }
-        }
-    }
-    else {
-        console.log("Id inválido!");
-    }
-};
-// Teste aqui QA
-// deletaItemImperativo(4)
-// console.log("Imperativo:\n", lista)
-// console.log("---------------------\n")
-// Teste aqui QA
-console.log("Crie uma função que altere a bio ou o name a partir de um id passado");
+deletaItem(1);
+console.log(lista);
 var modificaNome = function (identificador, nome) {
     // indice necessário, por isso a não utilização da verificaInidice
     var indice = lista.findIndex(function (objeto) { return objeto.id === identificador; });
@@ -123,21 +61,5 @@ var modificaNome = function (identificador, nome) {
         console.log("Id inválido!");
     }
 };
-// Teste aqui QA
-// modificaNome(2, "TESTANDO")
-// console.log("Funcional:\n", lista)
-var modificaNomeImperativo = function (identificador, nome) {
-    if (verificaIndice(identificador)) {
-        for (var i = 0; i < lista.length; i++) {
-            if (identificador === lista[i].id) {
-                lista[i].name = nome;
-            }
-        }
-    }
-    else {
-        console.log("Id inválido!");
-    }
-};
-// Teste aqui QA
-// modificaNomeImperativo(3, "TESTANDO IMPERATIVO")
-// console.log("Imperativo:\n", lista)
+modificaNome(2, "TESTANDO");
+console.log(lista);
--- jsScripts/ex-2-imperativo.js
+++ jsScripts/ex-2-imperativo.js
@@ -10,6 +10,7 @@ var lista = [
     { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
     { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
+// verifica se o id passado é válido
 var verificaIndice = function (identificador) {
     if ((lista.findIndex(function (objeto) { return objeto.id === identificador; })) != -1) {
         return true;
@@ -18,21 +19,6 @@ var verificaIndice = function (identificador) {
         return false;
     }
 };
-// Teste aqui QA
-// console.log("Crie uma função que retorne a bio do id passado \n")
-// Funcional
-var pegaBio = function (identificador) {
-    if (verificaIndice(identificador)) {
-        var objeto = lista.find(function (objeto) { return objeto.id === identificador; });
-        return objeto.bio;
-    }
-    else {
-        return "Id inválido!";
-    }
-};
-// Teste aqui QA
-// console.log("Funcional: ",pegaBio(1), "\n")
-// Imperativo
 var pegaBioImperativo = function (identificador) {
     if (verificaIndice(identificador)) {
         for (var i = 0; i < lista.length; i++) {
@@ -45,24 +31,7 @@ var pegaBioImperativo = function (identificador) {
         return "Id inválido!";
     }
 };
-// Teste aqui QA
-// console.log("Imperativo: ", pegaBioImperativo(1))
-// console.log("---------------------\n")
-// Teste aqui QA
-// console.log("Crie uma função que retorne o name do id passado\n")
-// Funcional
-var pegaNome = function (identificador) {
-    if (verificaIndice(identificador)) {
-        var objeto = lista.find(function (objeto) { return objeto.id === identificador; });
-        return objeto.name;
-    }
-    else {
-        return "Id inválido!";
-    }
-};
-// Teste aqui QA
-// console.log("Funcional: ",pegaNome(1), "\n")
-// Imperativo
+console.log(pegaBioImperativo(1));
 var pegaNomeImperativo = function (identificador) {
     if (verificaIndice(identificador)) {
         for (var i = 0; i < lista.length; i++) {
@@ -75,26 +44,7 @@ var pegaNomeImperativo = function (identificador) {
         return "Id inválido!";
     }
 };
-// Teste aqui QA
-// console.log("Imperativo: ", pegaNomeImperativo(1))
-// console.log("---------------------\n")
-// Teste aqui QA
-console.log("Crie uma função que apague um item da lista a partir de um id passado");
-// Funcional
-var deletaItem = function (identificador) {
-    // indice necessário, por isso a não utilização da verificaInidice
-    var indice = lista.findIndex(function (objeto) { return objeto.id === identificador; });
-    if (indice !== -1) {
-        lista.splice(indice, 1);
-    }
-    else {
-        console.log("Id inválido!");
-    }
-};
-// Teste aqui QA
-// deletaItem(1)
-// console.log("Funcional:\n", lista)
-// Imperativo
+console.log(pegaNomeImperativo(1));
 var deletaItemImperativo = function (identificador) {
     if (verificaIndice(identificador)) {
         for (var i = 0; i < lista.length; i++) {
@@ -107,25 +57,8 @@ var deletaItemImperativo = function (identificador) {
         console.log("Id inválido!");
     }
 };
-// Teste aqui QA
-// deletaItemImperativo(4)
-// console.log("Imperativo:\n", lista)
-// console.log("---------------------\n")
-// Teste aqui QA
-console.log("Crie uma função que altere a bio ou o name a partir de um id passado");
-var modificaNome = function (identificador, nome) {
-    // indice necessário, por isso a não utilização da verificaInidice
-    var indice = lista.findIndex(function (objeto) { return objeto.id === identificador; });
-    if (indice !== -1) {
-        lista[indice].name = nome;
-    }
-    else {
-        console.log("Id inválido!");
-    }
-};
-// Teste aqui QA
-// modificaNome(2, "TESTANDO")
-// console.log("Funcional:\n", lista)
+deletaItemImperativo(4);
+console.log(lista);
 var modificaNomeImperativo = function (identificador, nome) {
     if (verificaIndice(identificador)) {
         for (var i = 0; i < lista.length; i++) {
@@ -138,6 +71,5 @@ var modificaNomeImperativo = function (identificador, nome) {
         console.log("Id inválido!");
     }
 };
-// Teste aqui QA
-// modificaNomeImperativo(3, "TESTANDO IMPERATIVO")
-// console.log("Imperativo:\n", lista)
+modificaNomeImperativo(3, "TESTANDO IMPERATIVO");
+console.log(lista);
